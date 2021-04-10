ea0d9b5cf8f6c3bd6630a09b76a47db38f7be761
Justinn-Zamora <82248498+Justinn-Zamora@users.noreply.github.com>
2021-04-09 19:52:38 -0700

    Add files via upload

---
+++ Lab 0 Extra Credit.py
@@ -0,0 +1,13 @@
+## Lab 0, TMATH 307
+
+x = 10; y = 5
+_sum = x + y
+_difference = x - y
+_product = x*y
+_power = x**y
+_division = x/y
+
+print (_sum, _difference, _product, _power, _division)
+
+
+
